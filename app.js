813b7b655a2ef3b683d7a45c82bb13b9fd6d0913
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2018-07-15 21:16:48 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const Telegraf = require('telegraf');
+const config = require('./config')
+
+const bot = new Telegraf(config.apiKey)
+
+bot.command('start', ctx => {
+  return ctx.reply('Bot succesfully started!');
+})
+
+bot.hears('hi', ctx => {
+  return ctx.reply('Hey!, How are you?');
+})
+
+bot.startPolling();
