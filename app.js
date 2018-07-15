bf10e1b971b9fc8048ba0331e74a033283b877c7
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2018-07-15 22:26:48 +0530

    indentation

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const config = require('./config')
 const bot = new Telegraf(config.apiKey)
 
 bot.command('start', ctx => {
-  return ctx.reply('Bot succesfully started!');
+	return ctx.reply('Bot succesfully started!');
 })
 
 bot.hears('hi', ctx => {
-  return ctx.reply('Hey!, How are you?');
+	return ctx.reply('Hey!, How are you?');
 })
 
 bot.startPolling();
